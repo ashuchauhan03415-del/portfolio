d7e6fcca2422d2a0158bdfcf39ecf538659141c0
ashuchauhan03415-del <ashuchauhan03415@gmail.com>
2026-03-19 17:20:20 +0530

    Updated something

--- website/js/home.js
+++ website/js/home.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // NAVBAR SCROLL EFFECT
 
 window.addEventListener("scroll", function(){
@@ -74,81 +73,4 @@ localStorage.removeItem("loggedIn")
 
 window.location="login.html"
 
-=======
-// NAVBAR SCROLL EFFECT
-
-window.addEventListener("scroll", function(){
-
-let navbar = document.querySelector(".navbar");
-
-if(window.scrollY > 50){
-navbar.style.background = "#000";
-}else{
-navbar.style.background = "#111";
-}
-
-});
-
-
-
-
-// HERO BUTTON ANIMATION
-
-const btn = document.querySelector(".btn");
-
-btn.addEventListener("mouseover", function(){
-btn.style.transform = "scale(1.05)";
-});
-
-btn.addEventListener("mouseout", function(){
-btn.style.transform = "scale(1)";
-});
-
-
-// DARK MODE
-
-function toggleTheme(){
-
-document.body.classList.toggle("dark");
-
-if(document.body.classList.contains("dark")){
-localStorage.setItem("theme","dark");
-}else{
-localStorage.setItem("theme","light");
-}
-
-}
-
-if(localStorage.getItem("theme") === "dark"){
-document.body.classList.add("dark");
-}
-
-const reveals = document.querySelectorAll(".reveal");
-
-window.addEventListener("scroll",()=>{
-
-reveals.forEach(item=>{
-
-const top = item.getBoundingClientRect().top;
-const screen = window.innerHeight;
-
-if(top < screen-100){
-item.classList.add("active");
-}
-
-});
-
-});
-
-if(!localStorage.getitem("username")){
-    window.location="login.html"
-}
-
-function logout(){
-
-localStorage.removeItem("loggedIn")
-
-window.location="login.html"
-
->>>>>>> f371553c727b229df2dc773fafd70c1102aed835
 }
